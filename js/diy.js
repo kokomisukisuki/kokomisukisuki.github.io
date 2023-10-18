36ec27c1fa9fdee7edf6052c499ac9bf3bb50390
unknown <3230267087@qq.com>
2023-10-18 23:00:57 +0800

    Site updated: 2023-10-18 23:00:53

--- js/diy.js
+++ js/diy.js
@@ -1,90 +1,101 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-10-18 21:14:16
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-10-18 22:59:10
+ * @FilePath: \hexo\themes\butterfly\source\js\diy.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 //动态分类条
+var pathInfo = window.location.pathname;
 function categoriesBarActive() {
-    var urlinfo = window.location.pathname;
-    urlinfo = decodeURIComponent(urlinfo)
-    // console.log(urlinfo);
-    //判断是否是首页
-    if (urlinfo == '/') {
-      if (document.querySelector('#category-bar')) {
-        document.getElementById('首页').classList.add("select")
+  pathInfo = decodeURIComponent(pathInfo)
+  // console.log(pathInfo);
+  //判断是否是首页
+  if (pathInfo == '/') {
+    if (document.querySelector('#category-bar')) {
+      document.getElementById('首页').classList.add("select")
+    }
+  } else {
+    // 验证是否是分类链接
+    const pattern = /\/categories\/.*?\//;
+    let patbool = pattern.test(pathInfo);
+    let valuegroup = pathInfo.split("/");
+    for (let n = 0; n <= valuegroup.length; n++) {
+      n = valuegroup.length;
+      var nowCategorie = valuegroup[n - 2];
+    }
+    // console.log(patbool);
+    if (patbool) {
+      // console.log(valuegroup[2]);
+      if (document.getElementById(nowCategorie)) {
+        document.getElementById(nowCategorie).classList.add("select");
       }
     } else {
-      // 验证是否是分类链接
-      var pattern = /\/categories\/.*?\//;
-      var patbool = pattern.test(urlinfo);
-      var valuegroup = urlinfo.split("/");
-      for (var n = 0; n <= valuegroup.length; n++) {
-        n = valuegroup.length;
-        var nowCategorie = valuegroup[n - 2];
+      let id;
+      switch (nowCategorie) {
+        case 'game':
+          id = '小游戏';
+          break;
       }
-      // console.log(patbool);
-      if (patbool) {
-        // console.log(valuegroup[2]);
-        if (document.querySelector('#category-bar')) {
-          document.getElementById(nowCategorie).classList.add("select");
-        }
-      } else {
-        var id;
-        switch (nowCategorie) {
-          case 'game':
-            id = '小游戏';
-            break;
-        }
-        if (document.querySelector('#category-bar')) {
-          document.getElementById(id).classList.add("select");
-        }
+      if (document.querySelector('#category-bar')) {
+        document.getElementById(id).classList.add("select");
       }
     }
-    categoriesBarNext()
   }
-  //翻页按钮
-  function scrollCategoryBarToRight() {
-    var e = document.getElementById("category-bar-items")
-      , t = document.getElementById("category-bar-next")
-      , o = e.clientWidth;
-    e && (e.scrollLeft + e.clientWidth >= e.scrollWidth ? (e.scroll({
-      left: 0,
+  categoriesBarNext()
+}
+//翻页按钮
+function scrollCategoryBarToRight() {
+  var e = document.getElementById("category-bar-items")
+    , t = document.getElementById("category-bar-next")
+    , o = e.clientWidth;
+  e && (e.scrollLeft + e.clientWidth >= e.scrollWidth ? (e.scroll({
+    left: 0,
+    behavior: "smooth"
+  }),
+    t.innerHTML = '<i class="fa-solid fa-chevrons-right"></i>') : e.scrollBy({
+      left: o,
       behavior: "smooth"
-    }),
-      t.innerHTML = '<i class="fa-solid fa-chevrons-right"></i>') : e.scrollBy({
-        left: o,
-        behavior: "smooth"
-      }))
-  }
-  //新增方法：动态插入翻页按钮
-  function categoriesBarNext() {
-    if (document.getElementById("category-bar-items")) {
-      var e = document.getElementById("category-bar-items")
+    }))
+}
+//新增方法：动态插入翻页按钮
+function categoriesBarNext() {
+  if (document.getElementById("category-bar-items")) {
+    let e = document.getElementById("category-bar-items")
       , t = document.getElementById("category-bar-next")
-      if (e.scrollWidth > e.clientWidth) {
-        t.innerHTML = '<i class="fa-solid fa-chevrons-right"></i>'
-      } else {
-        t.remove('i')
-      }
+    if (e.scrollWidth > e.clientWidth) {
+      t.innerHTML = '<i class="fa-solid fa-chevrons-right"></i>'
+    } else if (t.children[0]) {
+      const iElement = document.querySelector('.category-bar-next i');
+      const parentElement = document.getElementById('category-bar-next');
+
+      parentElement.removeChild(iElement);
     }
   }
-  //窗口改变就执行
-  window.onresize = function() {
-    categoriesBarNext()
+}
+//窗口改变就执行
+window.onresize = function () {
+  categoriesBarNext()
+}
+
+//鼠标控制横向滚动
+function topCategoriesBarScroll() {
+  if (document.getElementById("category-bar-items")) {
+    let xscroll = document.getElementById("category-bar-items");
+    xscroll.addEventListener("mousewheel", function (e) {
+      //计算鼠标滚轮滚动的距离
+      let v = -e.wheelDelta / 2;
+      xscroll.scrollLeft += v;
+      //阻止浏览器默认方法
+      e.preventDefault();
+    }, false);
   }
-  
-  //鼠标控制横向滚动
-  function topCategoriesBarScroll() {
-    if (document.getElementById("category-bar-items")) {
-      let xscroll = document.getElementById("category-bar-items");
-      xscroll.addEventListener("mousewheel", function (e) {
-        //计算鼠标滚轮滚动的距离
-        let v = -e.wheelDelta / 2;
-        xscroll.scrollLeft += v;
-        //阻止浏览器默认方法
-        e.preventDefault();
-      }, false);
-    }
-  }
-  //方法调用
+}
+//方法调用
+categoriesBarActive()
+topCategoriesBarScroll()
+document.addEventListener("pjax:complete", (function () {
   categoriesBarActive()
   topCategoriesBarScroll()
-  document.addEventListener("pjax:complete", (function () {
-    categoriesBarActive()
-    topCategoriesBarScroll()
-  }))
+}))

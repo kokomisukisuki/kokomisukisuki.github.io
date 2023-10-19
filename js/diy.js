4853970eedcbde9f2606f199b6abfa2d8fe409e8
unknown <3230267087@qq.com>
2023-10-19 16:12:53 +0800

    Site updated: 2023-10-19 16:12:50

--- js/diy.js
+++ js/diy.js
@@ -1,11 +1,4 @@
-/*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2023-10-18 21:14:16
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2023-10-18 22:59:10
- * @FilePath: \hexo\themes\butterfly\source\js\diy.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+
 //动态分类条
 var pathInfo = window.location.pathname;
 function categoriesBarActive() {
